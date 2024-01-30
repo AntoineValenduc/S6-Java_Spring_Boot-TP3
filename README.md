81867eaf3b09b18a4150863fb66b7a333544852a
AntoineValenduc <ant.valenduc@gmail.com>
2024-01-30 10:38:24 +0100

    First commit - init project + config docker compose + update application.yml

--- README.md
+++
@@ -1,2 +0,0 @@
-# S6-Java_Spring_Boot-TP3
-M2i 2iTech - S6 : Java Spring Boot - TP3
---
+++ src/main/java/org/example/s6tp3cinema/S6Tp3CinemaApplication.java
@@ -0,0 +1,13 @@
+package org.example.s6tp3cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S6Tp3CinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S6Tp3CinemaApplication.class, args);
+    }
+
+}
